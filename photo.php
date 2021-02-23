41e8a985e22b18bc10aeb1141398961a4c3d0879
walid1999 <56828930+walid1999@users.noreply.github.com>
2021-02-23 10:26:55 +0100

    Update photo.php

--- photo.php
+++ photo.php
@@ -18,13 +18,14 @@ $annonce = $requeteSQL->fetch();
         <div class="card-body text-center mx-auto">
             <div class='cvp'>
                 <h3 class="card-title font-weight-bold">Titre : <?php echo $annonce['titre']; ?></h3>
-                <p class="Type">Type d'image : </p>
-                <p class="Produit">Produit : </p>
-                <p class="Humain">Humain : </p>
-                <p class="Institutionnelle">Institutionnelle : </p>
-                <p class="Format">Format : </p>
-                <p class="Credit">Crédits photos : </p>
-                <p class="Tags">Tags : </p>
+                <p class="Type">Type d'image : <?php echo $annonce['type']; ?> </p>
+                <p class="Humain">Humain : <?php echo $annonce['p_humain']; ?> </p>
+                <p class="Produit">Produit : <?php echo $annonce['p_produit']; ?></p>
+                <p class="Institutionnelle">Institutionnelle : <?php echo $annonce['p_instit']; ?> </p>
+                <p class="Format">Format : <?php echo $annonce['format']; ?> </p>
+                <p class="Credit">Crédits photos : <?php echo $annonce['credits']; ?> </p>
+                <p class="Tags">Droits : <?php echo $annonce['droits']; ?> </p>
+                <p class="Tags">Tags : <?php echo $annonce['tags']; ?> </p>
                 <button ><a href="modification.php?id=<?php echo $annonce['id_aliment']; ?>" style="text-decoration: none; color: black;">Modifier ou supprimer</a></button>
             </div>
         </div>
