ec57d0d501ed09d01225fe9da27857df7e445e0a
MMEZOUANI <marvin.mezouani@ynov.com>
2021-02-23 10:00:37 +0100

    v2

--- photo.php
+++ photo.php
@@ -17,8 +17,14 @@ $annonce = $requeteSQL->fetch();
     <div class="card mx-auto col-md-3 col-10 mt-5"> <img class='mx-auto img-thumbnail' src="<?php echo $annonce['img']; ?>" width="500" height="500" />
         <div class="card-body text-center mx-auto">
             <div class='cvp'>
-                <h5 class="card-title font-weight-bold"><?php echo $annonce['titre']; ?></h5>
-                
+                <h3 class="card-title font-weight-bold">Titre : <?php echo $annonce['titre']; ?></h3>
+                <p class="Type">Type d'image : </p>
+                <p class="Produit">Produit : </p>
+                <p class="Humain">Humain : </p>
+                <p class="Institutionnelle">Institutionnelle : </p>
+                <p class="Format">Format : </p>
+                <p class="Credit">Crédits photos : </p>
+                <p class="Tags">Tags : </p>
                 <button ><a href="modification.php?id=<?php echo $annonce['id_aliment']; ?>" style="text-decoration: none; color: black;">Modifier ou supprimer</a></button>
             </div>
         </div>
